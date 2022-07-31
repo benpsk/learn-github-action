36754cad318306d0af9d580a7156636d840749c8
Ben Gunn <97440301+ben-gunn@users.noreply.github.com>
2022-08-01 02:27:26 +0630

    patch-2 update add js for of.

--- app.js
+++ app.js
@@ -1,2 +1,65 @@
 let a = 'hello';
+
+let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+}
+
+let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+}
+
+let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+}
+
+let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+}
+
+let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+}
+
+let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+}
+
+let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+}
+
+let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+}
+
+let language = "JavaScript";
+
+let text = "";
+for (let x of language) {
+text += x;
+}
 console.log(a);
