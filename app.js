534ab89ed3f0d399a3502c3b05461fc95e2c20ca
Ben Gunn <97440301+ben-gunn@users.noreply.github.com>
2022-08-01 02:20:50 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 let a = 'hello';
+
+
+const cars = ["BMW", "Volvo", "Mini"];
+
+let text = "";
+for (let x of cars) {
+  text += x;
+}
+
+
+const cars = ["BMW", "Volvo", "Mini"];
+
+let text = "";
+for (let x of cars) {
+  text += x;
+}
 console.log(a);
