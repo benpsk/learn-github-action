bcf99b34148d6797212626047a0e4d489a7b13a6
Ben Gunn <bengunn.dev@gmail.com>
2022-08-01 23:40:49 +0630

    update v1-patch-1

--- dev.js
+++ dev.js
@@ -16,10 +16,13 @@ if (time < 10) {
 }
 
 
-if (time < 10) {
-  greeting = "Good morning";
-} else if (time < 20) {
-  greeting = "Good day";
-} else {
-  greeting = "Good evening";
+let hello = "Hello World";
+console.log(hello);
+
+
+const objs = { name: "John", age: 20, occupation: "Software engineer" };
+
+for (let obj of objs) {
+  console.log(obj.name);
 }
+
